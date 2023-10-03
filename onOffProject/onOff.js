861e2af59db8c3e5af0443c38c56197884bdc0ed
Rubi <rubipra812@gmail.com>
2023-10-03 15:59:12 +0530

    Done project 2

--- onOffProject/onOff.js
+++ onOffProject/onOff.js
@@ -1,12 +1,19 @@
 var comp = 0
-let button = document.querySelector(".click")
+let button = document.querySelector(".clickButton")
 button.addEventListener("click",() => {
-    button.textContent="Turn Off";
     if(comp===0){
-        button.textContent="Turn Off"
+        button.textContent="Turn On"
+        button.style.backgroundColor = "green"
+        let image = document.querySelector(".bulbImage")
+        image.setAttribute("src" , "/On-and-Off-Switch/bulb-on.png")
+        //  document.getElementsByClassName(".bulbImage")
+        //  .src="/On-and-Off-Switch/bulb-off.png"
         comp = 1
     }else{
-        button.textContent="Turn On"
+        button.textContent="Turn Off"
+        button.style.backgroundColor = "white"
+        let image = document.querySelector(".bulbImage")
+        image.setAttribute("src" , "/On-and-Off-Switch/bulb-off.png")
         comp = 0
     }
 })
